cb99d7fa8e1b5703b04d61dc21d0576a960a3579
ajbeach2 <ajbeach2@users.noreply.github.com>
2016-03-16 09:20:27 -0500

    update function
    fix broken regex, cleaner code :)

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-module.exports = function(source){
+module.exports = function(source) {
   this.cacheable();
-  var s = source;
-  var re = /"url":\s*\".*,/g;
-  var m;
-  do {
-    m = re.exec(source);
-    if (!!m) {
-      var path = m[0].split(':')[1].trim().slice(0, -1);
-      s = s.replace(path, 'require(' + path + ')');
-    }
-  } while (m);
-  return s;
+  return source.replace(/"url"\s*:\s*"([^"]*)"/g, function(match, path) {
+    return '"url":require("' + path + '")';
+  });
 }
